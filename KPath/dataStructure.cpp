f8a789e72185f80f25a70bf117dfde86c5289cd2
XiaopeiXu <xvxiaopeisjtu@gmail.com>
2016-04-24 20:05:25 -0500

    done

--- KPath/dataStructure.cpp
+++ KPath/dataStructure.cpp
@@ -64,12 +64,12 @@ void Graph::empty(){
 
 }
 
-bool Graph::insert(int u,int v,int w)
+bool Graph::insert(int u,int v,int w,bool d)
 {
     if(u>=vertices.size()||v>=vertices.size())return false;
 	if(exist(u,v)) return false;
 	vertices[u].addEdge(u,v,w);
-	vertices[v].addEdge(v,u,w);
+	if(d)vertices[v].addEdge(v,u,w);
 	numOfEdges++;
 
     return true;
@@ -86,4 +86,27 @@ int Graph::exist(int u,int v)const
 
 
 	//if(find(v.begin(), v.end(), val) != v.end())
+}
+
+Graph* Graph::toTopo(int * new_ver)
+{
+	int * toNewVer = new int[vertices.size()];
+	
+	for(int i=0;i<vertices.size();i++) toNewVer[new_ver[i]]=i;
+	Graph* res=new Graph(vertices.size());
+	for(int i=0;i<vertices.size();i++)
+	{
+		int v=new_ver[i];
+		//cout<<v<<endl;
+		vector<Edge>::iterator it = vertices[v].edges.begin();
+			//cout<<"??"<<endl;
+		while(it!=vertices[v].edges.end()){
+			if(toNewVer[it->end]>i) {res->insert(v,it->end,it->weight,false);}
+			//else it=vertices[v].edges.erase(it);
+			it++;
+		}
+			
+	}
+	return res;
+
 }
